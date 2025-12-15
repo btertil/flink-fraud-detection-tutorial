402b8fbd602f2d805fa7fc9f53c8672500102f44
Bartosz Tertil <bartosz.tertil@external.telekom.de>
2025-12-15 13:48:41 +0100

    demo version presented on 15th Dec 2025

--- src/main/java/simpleexample/SimpleFlinkJob.java
+++ src/main/java/simpleexample/SimpleFlinkJob.java
@@ -1,6 +1,7 @@
 package simpleexample;
 
 import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.util.Collector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;   
@@ -12,16 +13,20 @@ public class SimpleFlinkJob {
 		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         // Create a simple input data stream
-        DataStream<String> inputStream = env.fromElements("one", "two", "three", "four", "five",
-                "six", "seven", "eight", "nine", "ten");
+        DataStream<String> inputStream = env
+                .fromElements("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten")
+                .name("input-data");
 
         // Perform a simple transformation
         DataStream<Tuple2<String, Integer>> resultStream = inputStream
                 .map(value -> new Tuple2<>(value, value.length()))
-                .returns(Types.TUPLE(Types.STRING, Types.INT));
+                .returns(Types.TUPLE(Types.STRING, Types.INT))
+                .name("map-to-length");
 
         // Add the custom sink
-        resultStream.addSink(new CustomSink());
+        resultStream
+                .addSink(new CustomSink())
+                .name("custom-sink");
 
         // Execute the Flink job
         env.execute("Simple Flink Job");
